6bbb4c410a584ffcdcedfd9a7193e81764097e2a
sy <tndus1886@naver.com>
2025-01-21 11:16:07 +0900

    refactor: import 수정

--- src/main/java/org/example/expert/domain/todo/repository/TodoRepository.java
+++ src/main/java/org/example/expert/domain/todo/repository/TodoRepository.java
@@ -1,7 +1,5 @@
 package org.example.expert.domain.todo.repository;
 
-import java.time.LocalDate;
-import java.time.LocalDateTime;
 import org.example.expert.domain.todo.entity.Todo;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -9,6 +7,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
+import java.time.LocalDateTime;
 import java.util.Optional;
 
 public interface TodoRepository extends JpaRepository<Todo, Long> {
